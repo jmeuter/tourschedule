9ced220006e21e858e1e93fa890ca91693ba4a81
Jason Meuter <jmeuter@avail-tvn.com>
2014-10-18 16:03:09 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tourschedule::Application.config.secret_token = '02b975f1ac473e9b13477b28a662ab7ce4c766fb5474df6419936208a0420e136788a345230be998ceb06102db5e8b91eac00d8105e47bd4b1542a2f75274481'
